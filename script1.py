3979981903f224d915693c6cb549fc2b1932e19f
pankajkesri <156541463+pankajkesri@users.noreply.github.com>
2024-05-29 14:07:25 -0400

    created script files
    added code to print lines from Python's Zen library

---
+++ script1.py
@@ -0,0 +1,2 @@
+print("There should be one-- and preferably only  one --obvious way to do it.")
+print("Namespaces are one honking great idea -- let's do more of those!")
---
+++ script2.py
@@ -0,0 +1,2 @@
+print("Althrough that way ,may not be obivious at first  unnless you're Dutch.")
+print("If the implementation is easy to explain, it may be a good idea.")
